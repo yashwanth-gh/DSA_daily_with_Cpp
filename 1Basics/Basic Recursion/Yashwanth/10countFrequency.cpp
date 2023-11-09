88d12459429854a129973104c99f4b155a57d49d
Yashwanth B M <yashwanthbm36@gmail.com>
2023-11-09 11:12:15 +0530

    Updated folder Structure

--- 1Basics/Basic Recursion/Yashwanth/10countFrequency.cpp
+++
@@ -1,8 +0,0 @@
-vector<int> countFrequency(int n, int x, vector<int> &nums){
-    // Write your code here.
-    vector<int>v(n,0);
-    for(auto ele:nums){
-        v[ele-1]+=1;
-    }
-    return v;
-}
