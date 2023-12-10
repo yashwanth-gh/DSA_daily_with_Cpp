5dbe649f4d88edc912f56df41b0b2b5ddca3aba0
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-10 23:51:57 +0530

    attemted hard array Qs

---
+++ 03_Arrays/Yashwanth/Hard/01_PascalsTri.cpp
@@ -0,0 +1,35 @@
+// Brute force only upto N=22-----------
+// then it furtur causes overflow
+
+long long fac(int num){
+    if(num <= 1)return 1;
+    return num*fac(num-1);
+}
+
+vector<vector<int>> pascalTriangle(int N) {
+    // Write your code here.
+
+	vector<vector<int>>sol(N);
+    int formula = 0;
+
+    for (int i = 0; i < N; i++) {
+        int j = 0;
+        long long n = fac(i);
+
+        while (j <= i) {
+            if (i == 0) {
+                sol[i].push_back(1);
+                break;
+            }
+
+            long long r = fac(j);
+            long long sub = i - j;
+            long long q = fac(sub);
+
+            formula = static_cast<int>(n / (r * q)); // Ensure the result fits into an int
+            sol[i].push_back(formula);
+            j++;
+        }
+    }
+    return sol;
+}
